e2fd9c31720d07f7e7abd884bc51067c19130664
KevinP <kpradjinata@gmail.com>
2023-05-09 16:44:16 -0700

    commented all funcitons

--- indexer.py
+++ indexer.py
@@ -1,7 +1,5 @@
 import os
 import json
-import re
-from collections import defaultdict
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from bs4 import BeautifulSoup
@@ -13,9 +11,6 @@ class Indexer:
         self.ps = PorterStemmer()
 
     def index_document(self, doc_id, tokens):
-
-        #TODO CHECK DOC_ID
-
         # Create the inverted index
         for i in range(len(tokens)):
             if tokens[i] not in self.index:
@@ -26,21 +21,28 @@ class Indexer:
                 else:
                     self.index[tokens[i]][doc_id] += 1
 
+
     def load(self, path):
         # Load the index from a file
         with open(path, "r") as f:
             return json.load(f)
 
     def extract_words(self, json):
+        #load the HTML
         soup = BeautifulSoup(json["content"], "html.parser")
+
+        #fix broken HTML
         soup.prettify()
+
         # Tokenize the text
-        # print("ASDUHASUDHAISDAIS",type(soup))
         tokens = word_tokenize(soup.get_text())
+
         # Stem the remaining words
         stemTokens = [self.ps.stem(token.lower()) for token in tokens if token.isalnum()]
+
         return stemTokens
     
     def printindex(self):
+        #print the master index
         print(self.index)
     
